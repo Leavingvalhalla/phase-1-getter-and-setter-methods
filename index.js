829df74837258d621b30ae17ac747c75d19272b1
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-08 14:15:03 -0800

    commit tree 93c49ebafbefa3b904a5336cea8ca46a1b5fea82

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  get diameter() {
+    return 2 * this.radius;
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter / 2;
+  }
+
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+
+  set circumference(circumference) {
+    // prettier-ignore
+    const num = circumference / 2
+    this.radius = num / Math.PI;
+  }
+
+  get area() {
+    return Math.PI * this.radius ** 2;
+  }
+
+  set area(area) {
+    const num = area / Math.PI;
+    this.radius = Math.sqrt(num);
+  }
+}
